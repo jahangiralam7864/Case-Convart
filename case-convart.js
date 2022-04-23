72fb4a97d6e4f8ab1dcd96d5df43c7193affd61f
jahangiralam7864 <wedevs95@gmail.com>
2022-04-24 00:45:21 +0600

    case

---
+++ case-convart.js
@@ -0,0 +1,16 @@
+// Case check and toLowerCase check information
+const jahangir = "amar sonar bangla ami tomai valo basi";
+const JAHANGIR = "AMAR SONAR BANGLA AMI TOMAI VALO BASI";
+if (jahangir == JAHANGIR) {
+   console.log("you are all right");
+}
+else {
+    console.log("you are not right");
+}
+
+
+const userInput = "BLACKPINK";
+const savedUserName = "BlackPink";
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log("User exists");
+}
