3c8b257928efa55a13dc2ca52d764b3a2e5618e0
jahangiralam7864 <wedevs95@gmail.com>
2022-04-24 02:11:14 +0600

    last try

---
+++ slice.js
@@ -0,0 +1,19 @@
+const bangladesh = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const words = bangladesh.split(' ');
+// result : [ 'Amar', 'Sonar', 'Bangla', 'Ami', 'Tomai', 'Valobashi' ]
+const withoutA = bangladesh.split('a');
+// result : [
+//  [ 'Am',       'r Son',
+//   'r B',      'ngl',
+//   ' Ami Tom', 'i V',
+//   'lob',      'shi'
+// ]
+const smallSlice = bangladesh.slice(5, 13);
+// result : Sonar Ba // 5 theke suru kore 13 ager sonkha pawa jabe
+const anotherPart = bangladesh.substr(11, 6);
+// result : Bangla  sbustr sue kore amra koto number index theke suru korbo, than koto sonkha pojonto count korbo seta khuje dibe.
+const AnotherPart = bangladesh.substring(11, 15);
+// result : substring diye amara kono kisur man koto number index theke suru hobe , than koto number index ar age pojonto print hobe seta dekhano holo Bang
+
+console.log(AnotherPart);
+// sob gulo mathord k check korty amra just console a variable ta change kore try korbo.
