96fa26203625f31db6c8b6affddcd573c9471cbe
jahangiralam7864 <wedevs95@gmail.com>
2022-04-24 01:46:14 +0600

    submit

--- string-seach.js
+++ string-seach.js
@@ -1,4 +1,4 @@
-const product = [
+const products = [
     'dell hardcore i19 200gb laptop',
     'iphone amar onek posondo',
     'black color amar onek posondo',
@@ -6,10 +6,29 @@ const product = [
     'phone amar valo lage na kamon lage bolo to'
 ];
 const searching = 'laptop';
-// indexOf
+// indexOf diye j contant search kora hobe shudu sei sonkha search kore return korbe
+
 const output = [];
 for (const product of products){
-    if (product.to>toLowerCase().indexOf(searching .toLowerCase()) != -1) {
+    if (product.toLowerCase().indexOf(searching .toLowerCase()) != -1) {
+        output.push(product);
+    }
+}
+console.log(output);
+
+// kono array jodi sob gulo document print korty cai taile amra includes use korbo
+const output = [];
+for (const product of products){
+    if (product.toLowerCase().includes(searching .toLowerCase()) != -1) {
+        output.push(product);
+    }
+}
+console.log(output);
+
+// startWirh same kaj kore thake includes ar moto
+const output = [];
+for (const product of products){
+    if (product.toLowerCase().startsWith(searching .toLowerCase()) != -1) {
         output.push(product);
     }
 }
